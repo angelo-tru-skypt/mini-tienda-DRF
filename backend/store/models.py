0e2ab7633ad798c1830f1b6fa913fe66ddc5a731
Angelo-dev <angelomancebo8@gmail.com>
2025-11-03 18:59:36 -0400

    Cambios ligeros

--- backend/store/models.py
+++ backend/store/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
@@ -7,5 +6,3 @@ class Product(models.Model):
     image = models.ImageField(upload_to='products/', null=True, blank=True)
     def __str__(self):
         return self.name;
-
-
