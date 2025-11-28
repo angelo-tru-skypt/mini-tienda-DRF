17095e1dd7f08e9f7be148bd2ccf489ca46d8429
Angelo-dev <angelomancebo8@gmail.com>
2025-11-27 21:36:59 -0400

    Actucalización de vistas con APIView para la utilización de poo. Y mantener mejores practicas y robustes

--- backend/login/urls.py
+++ backend/login/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import register_user, login_user
+from .views import LoginUserAPIView, RegisterUserAPIView
 
 urlpatterns = [
-    path("users/register/", register_user, name="register"),
-    path("users/login/", login_user, name="login")
+    path("users/register/", RegisterUserAPIView.as_view(), name="register"),
+    path("users/login/", LoginUserAPIView.as_view(), name="login")
 ]
--- backend/login/views.py
+++ backend/login/views.py
@@ -1,46 +1,50 @@
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
 from .models import Users
+from rest_framework.views import APIView
 from .serializers import UsersSerializers
 
-@api_view(['POST'])
-def register_user(request):
-    email = request.data.get('email')
-    if Users.objects.filter(email=email).exists():
-        return Response({'error': 'El usuario ya existe'}, status=status.HTTP_400_BAD_REQUEST)
+class RegisterUserAPIView(APIView):
+    def post(self, request):
+        email = request.data.get('email')
 
-    serializer = UsersSerializers(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response({'mensaje': 'Usuario creado correctamente'}, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if Users.objects.filter(email=email).exists():
+            return Response({"error", "El usuario ya existe"}, status=status.HTTP_400_BAD_REQUEST)
 
+        serializer = UsersSerializers(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"msg": "Usuario creado correctamene"}, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
 
-@api_view(['POST'])
-def login_user(request):
-    email = request.data.get('email')
-    password = request.data.get('password')
+class LoginUserAPIView(APIView):
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
 
-    try:
-        usuario = Users.objects.get(email=email)
-    except Users.DoesNotExist:
-        return Response({'error': 'Usuario no encontrado'}, status=status.HTTP_404_NOT_FOUND)
+        try:
+            usuario = Users.objects.get(email=email)
+        except Users.DoesNotExist:
+            return Response({"error": "Usuario no encontrado"}, status=status.HTTP_404_NOT_FOUND)
+        
+        if usuario.verificar_password(password):
+            refresh = RefreshToken.for_user(usuario)
+            return Response({
+                'mensaje': 'Login exitoso',
+                'usuario': {
+                    'id': usuario.id,
+                    'username': usuario.username,
+                    'email': usuario.email
+                },
+                'tokens': {
+                    'refresh': str(refresh),
+                    'access': str(refresh.access_token),
+                }
+            }, status=status.HTTP_200_OK)
 
-    if usuario.verificar_password(password):
-        refresh = RefreshToken.for_user(usuario)
-        return Response({
-            'mensaje': 'Login exitoso',
-            'usuario': {
-                'id': usuario.id,
-                'username': usuario.username,
-                'email': usuario.email
-            },
-            'tokens': {
-                'refresh': str(refresh),
-                'access': str(refresh.access_token),
-            }
-        }, status=status.HTTP_200_OK)
-
-    return Response({'error': 'Contraseña incorrecta'}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response(
+            {'error': 'Contraseña incorrecta'},
+            status=status.HTTP_401_UNAUTHORIZED
+        )
